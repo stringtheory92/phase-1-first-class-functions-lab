16a0febfeb98eefa3389512cc243eefa442381b1
Adam Kabak <adamkabak@yahoo.com>
2022-09-19 20:50:28 -0400

    commit tree 8024b8665611ee2f6b3ad7fe478e17b99a1d6d17

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => array.slice(0, 2);
+
+const returnLastTwoDrivers = (array) => array.slice((array.length - 2))
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return fare => int * fare;
+}
+
+function fareDoubler(fare) {
+    const multipliedFare = createFareMultiplier(2);
+    return multipliedFare(fare);
+}
+
+function fareTripler(fare) {
+    const multipliedFare = createFareMultiplier(3);
+    return multipliedFare(fare);
+}
+
+function selectDifferentDrivers(array, cb) {
+    const candidates = cb(array);
+    return candidates;
+}
